c71ec7a715124474b9faaae35a2a6d9f464ecbd1
Omar Makram Eldeeb <61871078+Omar-Eldeeb98@users.noreply.github.com>
2025-03-29 02:54:09 +0200

    29 -3
    do some updates ,
    add new pages

--- src/app/pages/about-pages/certifications/certifications.component.ts
+++ src/app/pages/about-pages/certifications/certifications.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-certifications',
   templateUrl: './certifications.component.html',
-  styleUrls: ['./certifications.component.scss']
+  styleUrls: ['./certifications.component.scss'],
 })
 export class CertificationsComponent {
+  certifications: any[] = [
+    {
+      title: 'typescript',
+      img: '../../../../assets/images/certifications/typescript.jpg',
+      link: 'https://drive.google.com/file/d/1xkHb6aJq8HT5yK7qwYFa6F9BVGXQ-ds3/view?usp=sharing',
+    },
+    {
+      title: 'java ',
+      img: '../../../../assets/images/certifications/java.png',
+      link: 'https://www.sololearn.com/certificates/CT-CXQN82RJ',
+    },
+    {
+      title: 'angular',
+      img: '../../../../assets/images/certifications/angular.png',
+      link: 'https://www.sololearn.com/certificates/CC-FVRWFYXH',
+    },
 
+    {
+      title: 'java se 8',
+      img: '../../../../assets/images/certifications/javase8.jpg',
+      link: 'https://drive.google.com/file/d/19d2p1iG8Xei4cZL5clzf6s2987HUYSpt/view?usp=sharing',
+    },
+    {
+      title: 'oop',
+      img: '../../../../assets/images/certifications/oop.jpg',
+      link: 'https://drive.google.com/file/d/16lz0Z0vmN5CVqmWj9rCN0y7yG3rs7B3y/view?usp=sharing',
+    },
+    {
+      title: 'my sql',
+      img: '../../../../assets/images/certifications/mysql.jpg',
+      link: 'https://drive.google.com/file/d/1X2y8SCbxc2nqJO2nhgSOL-G3RVxT37z7/view?usp=sharing',
+    },
+    {
+      title: 'android dev',
+      img: '../../../../assets/images/certifications/android.jpg',
+      link: 'https://drive.google.com/file/d/12RLWCD0p-TtMUIO-ICYgI9Ms-mxX5oGF/view?usp=sharing',
+    },
+  ];
 }
--- src/app/pages/about-pages/courses/courses.component.ts
+++ src/app/pages/about-pages/courses/courses.component.ts
@@ -3,8 +3,81 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  courses: any[] = [];
 
+  constructor() {
+    this.courses = [
+      {
+        img: '../../../../assets/images/courses/route.png',
+        academy: 'Route Academy (software training)',
+        date: 'oct 2023',
+        courseName: 'front-end web development by angular framework.',
+        duration: '6 months.',
+      },
+      {
+        img: '../../../../assets/images/courses/route.png',
+        academy: 'Route Academy (software training)',
+        date: 'may 2024',
+        courseName: '.net back-end web development.',
+        duration: '5 months.',
+      },
+      {
+        img: '../../../../assets/images/courses/mahara.png',
+        academy: 'mahara-tech (iti platform)',
+        date: 'mar 2025',
+        courseName:
+          'typescript : practical understanding of typescript programming language.',
+        duration: '',
+      },
+      {
+        img: '../../../../assets/images/courses/sololearn.png',
+        academy: 'sololearn (programming platform)',
+
+        date: 'mar 2025',
+        courseName:
+          ' Angular : practical understanding of angular 2+ framework.',
+        duration: '',
+      },
+      {
+        img: '../../../../assets/images/courses/sololearn.png',
+        academy: 'sololearn (programming platform)',
+        date: 'jan 2021',
+        courseName:
+          ' java : theorical and practical understanding of java programming language.',
+        duration: '',
+      },
+
+      {
+        img: '../../../../assets/images/courses/nh.png',
+        academy: 'new horizons (computer learning centers)',
+        date: 'july 2019',
+        courseName: 'java se 8 : fundamentals of programming.',
+        duration: 'this is an 40 hours course.',
+      },
+      {
+        img: '../../../../assets/images/courses/nh.png',
+        academy: 'new horizons (computer learning centers)',
+        date: 'sep 2019',
+        courseName: 'java se 8 : object oriented programming.',
+        duration: 'this is an 40 hours course.',
+      },
+      {
+        img: '../../../../assets/images/courses/nh.png',
+        academy: 'new horizons (computer learning centers)',
+        date: 'sep 2019',
+        courseName: 'fundamentals of mySQL.',
+        duration: 'this is an 24 hours course.',
+      },
+      {
+        img: '../../../../assets/images/courses/nh.png',
+        academy: 'new horizons (computer learning centers)',
+        date: 'feb 2020',
+        courseName: 'android applications development mySQL.',
+        duration: 'this is an 80 hours course.',
+      },
+    ];
+  }
 }
